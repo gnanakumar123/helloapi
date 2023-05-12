549dfcddf6d8cb950ecaae30db32539946b00866
gnanakumar123 <gnana.kumar123@gmail.com>
2023-05-12 11:18:19 +0530

    initial files

--- src/main/java/com/example/helloapi/SwaggerConfig.java
+++ src/main/java/com/example/helloapi/SwaggerConfig.java
@@ -1,14 +1,10 @@
 package com.example.helloapi;
 
-import java.util.Collections;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
+import java.util.*;
+
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -30,14 +26,5 @@ public class SwaggerConfig {
 		return new ApiInfo("Spring Boot Example Swagger", "Here some description of API.", "API 1.0",
 				"Terms of service", new Contact("My name", "www.mydomain.com", "myeaddress@mydomain.com"),
 				"License of API", "API license URL", Collections.emptyList());
-	}
-
-	@Bean
-	public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
-		return new OpenAPI().info(new Info().title("Controller API").version(appVersion)
-				.description(
-						"This is a sample server created using springdocs - a library for OpenAPI 3 with spring boot.")
-				.termsOfService("http://swagger.io/terms/")
-				.license(new License().name("Apache 2.0").url("http://springdoc.org")));
 	}
 }
